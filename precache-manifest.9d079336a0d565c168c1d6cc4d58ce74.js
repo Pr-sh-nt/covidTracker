aaf051c6d8484d9927308d32d4f1edd6f05de4a7
Pr-sh-nt <pk777.paras@gmail.com>
2020-06-14 19:31:19 +0530

    Updates

---
+++ precache-manifest.9d079336a0d565c168c1d6cc4d58ce74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37cb962d006165442240111459b80980",
+    "url": "/covidTracker/index.html"
+  },
+  {
+    "revision": "086f70e9a9e6638e17cb",
+    "url": "/covidTracker/static/css/main.d177187b.chunk.css"
+  },
+  {
+    "revision": "8ddae5db726b1d87867a",
+    "url": "/covidTracker/static/js/2.0b2280e9.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidTracker/static/js/2.0b2280e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "086f70e9a9e6638e17cb",
+    "url": "/covidTracker/static/js/main.32408939.chunk.js"
+  },
+  {
+    "revision": "fe798c1308901815c581",
+    "url": "/covidTracker/static/js/runtime-main.a2d797a4.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covidTracker/static/media/image.d7265326.png"
+  }
+]);
